c774eacf4cd1cd7a6adc2de94df3e18121672bda
Dan Parkes <daniel.parkes901@gmail.com>
2017-04-27 16:51:12 +0100

    got jquery working

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-$(function(){
- $("#quiz").click(function(){
-          $(quiz).addClass(btn-basic).removeClass(btn-basic)};
-      });
-      
+var score = 0;
+$(document).ready(function(){
+    $(".correct").click(function(){
+         $(this).removeClass("btn-primary").addClass("answer_correct").off("click");
+         $(this).siblings().removeClass("btn-primary").off("click");
+         score+1;
+    }); 
+     $(".incorrect").click(function(){
+         $(this).removeClass("btn-primary").addClass("answer_incorrect").off("click");
+         $(this).siblings().removeClass("btn-primary").off("click");
+    }); 
+    
 });	
 
 
