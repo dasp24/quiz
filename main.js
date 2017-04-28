a496ee222c71df66fb6e299476f89496d8ec19b9
Dan Parkes <daniel.parkes901@gmail.com>
2017-04-28 12:33:03 +0100

    more questions and moving scoreboard wtf

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var score = 0;
+
 $(document).ready(function(){
+    var score = 0;
+    runningTotal=document.getElementById('Total')
+    runningTotal.innerHTML = "score: "+ score;
     $(".correct").click(function(){
          $(this).removeClass("btn-primary").addClass("answer_correct").off("click");
          $(this).siblings().removeClass("btn-primary").off("click");
-         score+1;
+         score +=1;
+         return runningTotal.innerHTML = "score: "+ score;
     }); 
      $(".incorrect").click(function(){
          $(this).removeClass("btn-primary").addClass("answer_incorrect").off("click");
