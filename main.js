8d5d219dfbe30b0b7afeca7989b67a2db7c5ba0c
Dan Parkes <daniel.parkes901@gmail.com>
2017-04-28 16:23:40 +0100

    hide now works, questions are all there

--- main.js
+++ main.js
@@ -1,40 +1,38 @@
 
-$(document).ready(function(){
-    
-//   $('.questions').hide();
-//   $('.answers').hide();
-//   $('#question-1').show();
-//   $('#answer-1').show();
+$(document).ready(function () {
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
+      $('.questions').hide();
+      $('#question-1').show();
+
+
+
+
+
+
+
+
+
+
     var score = 0;
-    runningTotal=document.getElementById('Total')
-    runningTotal.innerHTML = "score: "+ score;
-    $(".correct").click(function(){
-         $(this).removeClass("btn-primary").addClass("btn-success").off("click");
-         $(this).siblings().removeClass("btn-primary").off("click");
-         score +=1;
-        //  $(this).parents().next('.questions').show();
-        //  $(this).parents().next().contents('.answers').show();
-         
-         return runningTotal.innerHTML = "score: "+ score +"/10";
-    }); 
-     $(".incorrect").click(function(){
-         $(this).removeClass("btn-primary").addClass("btn-danger").off("click");
-         $(this).siblings().removeClass("btn-primary").off("click");
-        //  $(this).parents().next('.questions').show();
-        //  $(this).parents().next('.answers').show();
-    }); 
-    
-});	
+    runningTotal = document.getElementById('Total')
+    runningTotal.innerHTML = "score: " + score;
+    $(".correct").click(function () {
+        $(this).removeClass("btn-primary").addClass("btn-success").off("click");
+        $(this).siblings().removeClass("btn-primary").off("click");
+        score += 1;
+         $(this).parents().next('.questions').show();
+        
+
+        return runningTotal.innerHTML = "score: " + score + "/10";
+    });
+    $(".incorrect").click(function () {
+        $(this).removeClass("btn-primary").addClass("btn-danger").off("click");
+        $(this).siblings().removeClass("btn-primary").off("click");
+         $(this).parents().next('.questions').show();
+        
+    });
+
+});
 
 
 
