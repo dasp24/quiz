5663d412fe885c65f9d2b54d852a28647c0bff0e
Dan Parkes <daniel.parkes901@gmail.com>
2017-04-28 15:46:51 +0100

    added content

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 
 $(document).ready(function(){
+    
+//   $('.questions').hide();
+//   $('.answers').hide();
+//   $('#question-1').show();
+//   $('#answer-1').show();
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
     var score = 0;
     runningTotal=document.getElementById('Total')
     runningTotal.innerHTML = "score: "+ score;
     $(".correct").click(function(){
-         $(this).removeClass("btn-primary").addClass("answer_correct").off("click");
+         $(this).removeClass("btn-primary").addClass("btn-success").off("click");
          $(this).siblings().removeClass("btn-primary").off("click");
          score +=1;
-         return runningTotal.innerHTML = "score: "+ score;
+        //  $(this).parents().next('.questions').show();
+        //  $(this).parents().next().contents('.answers').show();
+         
+         return runningTotal.innerHTML = "score: "+ score +"/10";
     }); 
      $(".incorrect").click(function(){
-         $(this).removeClass("btn-primary").addClass("answer_incorrect").off("click");
+         $(this).removeClass("btn-primary").addClass("btn-danger").off("click");
          $(this).siblings().removeClass("btn-primary").off("click");
+        //  $(this).parents().next('.questions').show();
+        //  $(this).parents().next('.answers').show();
     }); 
     
 });	
