d65ff42bd3e13868b4d1d7c3a1d0c7a1feb5e0d4
Dan Parkes <daniel.parkes901@gmail.com>
2017-05-05 14:54:11 +0100

    fixed a bug

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ $(document).ready(function () {
 
 
     var score = 0;
-   
+    $('.correct').on("click", function () {
+        if (score >= 9) {
+            $("#picture").attr("src", "http://zubeezone.com/blog/wp-content/uploads/2015/02/picard-winning1.png");
+        }
+        else if (score >= 4) {
+            $("#picture").attr("src", "http://c86og3avv551mqtcy2adcf845a.wpengine.netdna-cdn.com/wp-content/uploads/2016/12/charlie-sheen-not-winning.jpg");
+        }
+    })
+
     runningTotal = document.getElementById('Total')
     runningTotal.innerHTML = "score: " + score;
     $(".correct").click(function () {
@@ -14,22 +22,17 @@ $(document).ready(function () {
         $(this).siblings().removeClass("btn-primary").off("click");
         score += 1;
         $(this).parents().next('.questions').show();
-        $("html,body").animate({ scrollTop:$(document).height() }, "slow");
-        $('.correct').on("click", function () {
-    if (score >= 9) {
-            $("#picture").attr("src", "http://zubeezone.com/blog/wp-content/uploads/2015/02/picard-winning1.png");
-        }
-    else if (score >= 4) {
-            $("#picture").attr("src", "http://c86og3avv551mqtcy2adcf845a.wpengine.netdna-cdn.com/wp-content/uploads/2016/12/charlie-sheen-not-winning.jpg");
-        }
-    
+        $("html,body").animate({ scrollTop: $(document).height() }, "slow");
         return runningTotal.innerHTML = "score: " + score + "/10";
+
+
+
     });
     $(".incorrect").click(function () {
         $(this).removeClass("btn-primary").addClass("btn-danger").off("click");
         $(this).siblings().removeClass("btn-primary").off("click");
         $(this).parents().next('.questions').show();
-        $("html,body").animate({ scrollTop:$(document).height() }, "slow");
-    }); 
-    })
+        $("html,body").animate({ scrollTop: $(document).height() }, "slow");
+    });
+
 });
